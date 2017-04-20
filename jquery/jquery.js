bbd1cd99d66690ce882043ee928c7e6b22f9e152
Ben Richards <richards.ben@live.com>
2017-04-20 13:39:14 +0100

    Fixed Sliders on page
    The clients current website has images of accommodation, but is not up
    to date with the content on his website. Therefore, the first two
    sliders work fine but the third has no images yet as there are no
    images to show yet.

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,40 +1,103 @@
 jQuery(document).ready(function ($) {
   
-	var slideCount = $('#slider ul li').length;
-	var slideWidth = $('#slider ul li').width();
-	var slideHeight = $('#slider ul li').height();
+	var slideCount = $('.slider ul li').length;
+	var slideWidth = $('.slider ul li').width();
+	var slideHeight = $('.slider ul li').height();
 	var sliderUlWidth = slideCount * slideWidth;
 	
-	$('#slider').css({ width: slideWidth, height: slideHeight });
+	$('.slider').css({ width: slideWidth, height: slideHeight });
 	
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	$('.slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
 	
-    $('#slider ul li:last-child').prependTo('#slider ul');
+    $('#slider1 ul li:last-child').prependTo('#slider1 ul');
 
-    function moveLeft() {
-        $('#slider ul').animate({
+    function moveLeftSlider1() {
+        $('#slider1 ul').animate({
             left: + slideWidth
         }, 200, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
+            $('#slider1 ul li:last-child').prependTo('#slider1 ul');
+            $('#slider1 ul').css('left', '');
         });
     };
 
-    function moveRight() {
-        $('#slider ul').animate({
+    function moveRightSlider1() {
+        $('#slider1 ul').animate({
             left: - slideWidth
         }, 200, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
+            $('#slider1 ul li:first-child').appendTo('#slider1 ul');
+            $('#slider1 ul').css('left', '');
         });
     };
 
-    $('a.control_prev').click(function () {
-        moveLeft();
+    $('#prev1').click(function () {
+        moveLeftSlider1();
     });
 
-    $('a.control_next').click(function () {
-        moveRight();
+    $('#next1').click(function () {
+        moveRightSlider1();
+    });
+  
+  
+  
+  
+  $('#slider2 ul li:last-child').prependTo('#slider2 ul');
+  
+  function moveLeftSlider2() {
+        $('#slider2 ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider2 ul li:last-child').prependTo('#slider2 ul');
+            $('#slider2 ul').css('left', '');
+        });
+    };
+
+    function moveRightSlider2() {
+        $('#slider2 ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider2 ul li:first-child').appendTo('#slider2 ul');
+            $('#slider2 ul').css('left', '');
+        });
+    };
+
+    $('#prev2').click(function () {
+        moveLeftSlider2();
+    });
+
+    $('#next2').click(function () {
+        moveRightSlider2();
+    });
+
+  
+  
+  
+  
+  $('#slider3 ul li:last-child').prependTo('#slider3 ul');
+  
+    function moveLeftSlider3() {
+        $('#slider3 ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider3 ul li:last-child').prependTo('#slider3 ul');
+            $('#slider3 ul').css('left', '');
+        });
+    };
+
+    function moveRightSlider3() {
+        $('#slider3 ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider3 ul li:first-child').appendTo('#slider3 ul');
+            $('#slider3 ul').css('left', '');
+        });
+    };
+
+    $('#prev3').click(function () {
+        moveLeftSlider3();
+    });
+
+    $('#next3').click(function () {
+        moveRightSlider3();
     });
 
 });    
